8727c8c089b19b934c6a91a1d6bfd42de0a4c3af
Janet Lee <janle@microsoft.com>
2019-06-26 05:38:26 +0000

    Allow az login using managed identity.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
-echo logging in as ${AZ_CLIENT_ID}
-az login --service-principal --username  ${AZ_CLIENT_ID} --password ${AZ_CLIENT_SECRET} --tenant ${AZ_TENANT_ID}
 
-echo setting subscription
-az account set --subscription ${AZ_SUBSCRIPTION_ID}
+if [ -z ${AZ_CLIENT_ID} ]; then
+    # n.b. https://github.com/Azure/azure-cli/issues/9537
+    echo "logging in with managed identity"
+    az login --identity
+else
+    echo logging in as ${AZ_CLIENT_ID}
+    az login --service-principal --username  ${AZ_CLIENT_ID} --password ${AZ_CLIENT_SECRET} --tenant ${AZ_TENANT_ID}
+    echo setting subscription
+    az account set --subscription ${AZ_SUBSCRIPTION_ID}
+fi
 
 exec "$@"
